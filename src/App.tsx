f9547017af7ca815d7ec7a6936423e6fbd087480
Vitória Dourado <vickawaii14@gmail.com>
2024-10-09 21:24:30 -0300

    :tada: init: Inicializando projeto com Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h3 className='text-orange-600'>oiiii</h3>
+      </div>
+    </>
+  );
+}
+
+export default App
